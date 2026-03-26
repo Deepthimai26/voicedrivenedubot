49300ece66c09f2f0c7bcdc06d70afa5f853a9df
Deepthi Mai <158718323+Deepthimai26@users.noreply.github.com>
2026-03-26 10:35:50 +0530

    Obfuscate Firebase config valuesapi key
    Replaced sensitive Firebase configuration values with placeholders.

--- voice-driven-edubot/src/lib/firebaseConfig.js
+++ voice-driven-edubot/src/lib/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPVCJuHT3YqA9kWJZ5uT_snMv3wMI6mj0",
-  authDomain: "voice-driven-edubot.firebaseapp.com",
-  projectId: "voice-driven-edubot",
-  storageBucket: "voice-driven-edubot.firebasestorage.app",
-  messagingSenderId: "35327540994",
-  appId: "1:35327540994:web:8aa62a0dd1393ec4380c1b",
-  measurementId: "G-BXZ4EKF6MR"
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*t",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*",
+  measurementId: "*"
 };
 const app = initializeApp(firebaseConfig);
 
